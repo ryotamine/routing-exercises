69224e96fc3c3c1f7fefea4321a435049f9f2a06
Ryota Mine <ryotamine@gmail.com>
2019-06-28 18:02:46 +0000

    Use body parser for all routes

--- express/express_server.js
+++ express/express_server.js
@@ -4,6 +4,9 @@ const PORT    = 8080;
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // GET home page
 app.get("/", (req, res) => {
   res.render("urls_index");
