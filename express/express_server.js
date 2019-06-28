cc4fbc890ea3c323dd113aca70d0e5fd58e49dd5
Ryota Mine <ryotamine@gmail.com>
2019-06-28 18:52:06 +0000

    Comment express server code

--- express/express_server.js
+++ express/express_server.js
@@ -5,13 +5,16 @@ const bcrypt        = require("bcrypt");
 const app  = express();
 const PORT = 8080;
 
+// Use cookie session for login event
 app.use(cookieSession({
   name: "session",
   keys: ["express"]
 }));
 
+// Set ejs as the template engine
 app.set("view engine", "ejs");
 
+// Use body parser for all routes
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,6 +23,7 @@ app.get("/", (req, res) => {
   res.render("urls_index");
 });
 
+// Boot server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
