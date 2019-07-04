7ddf754f04a027d58f20618553786f1b55b7f5dc
Ryota Mine <ryotamine@gmail.com>
2019-07-04 20:29:29 +0000

    Set users database

--- express/migrations/20190704200735_users.js
+++ express/migrations/20190704200735_users.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments('id').primary();
+    table.string('first_name');
+    table.string('last_name');
+    table.string('email');
+    table.string('password');
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('users');
 };
