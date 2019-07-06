6956943e93646a7e6472f0f7845f2960aa31e649
Ryota Mine <ryotamine@gmail.com>
2019-07-06 23:28:03 +0000

    Create register model

--- rails/app/models/rails_user.rb
+++ rails/app/models/rails_user.rb
@@ -1,2 +1,16 @@
 class RailsUser < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }
+  has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user_found = RailsUser.find_by email: email
+    if user_found && user_found.authenticate(password)
+      user_found
+    else
+      nil
+    end
+  end
 end
