7a4ba6b21b77067fc25396bb50c3c7332778856e
Ryota Mine <ryotamine@gmail.com>
2019-07-06 20:19:30 +0000

    Create rails users database

--- rails/db/migrate/20190706200455_create_rails_users.rb
+++ rails/db/migrate/20190706200455_create_rails_users.rb
@@ -1,7 +1,10 @@
 class CreateRailsUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :rails_users do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
       t.timestamps
     end
   end
