a2dce6ebece02ea114840078294b221e940ce63d
Ryota Mine <ryotamine@gmail.com>
2019-07-06 22:47:17 +0000

    Set login page skeleton template

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "rails_users#index"
-  get "/register", to: "rails_users#register"
+  get "/register" => "rails_users#register"
+  get "/login" => "rails_users#login"
 end
