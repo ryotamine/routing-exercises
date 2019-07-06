dd3020dbb6112baaeede7c8c31210a1ded1d5f2e
Ryota Mine <ryotamine@gmail.com>
2019-07-06 23:40:41 +0000

    Create register controller

--- rails/app/controllers/rails_users_controller.rb
+++ rails/app/controllers/rails_users_controller.rb
@@ -1,2 +1,30 @@
 class RailsUsersController < ApplicationController
+  def index
+    @users = RailsUser.order(id: :desc).all
+  end
+
+  def new
+  end
+
+  def create
+    @user = RailsUser.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/welcome"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password
+    )
+  end
 end
