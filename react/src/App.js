61988d4f1330c63e6abcd82c50dc811428ab2605
Ryota Mine <ryotamine@gmail.com>
2019-07-06 18:10:21 +0000

    Create react links

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+// Home function
+function Home() {
+  return <h3>Home</h3>;
+}
+
+// About function
+function About() {
+  return <h3>Ryota Mine</h3>;
+}
+
+// Contact function
+function Contact() {
+  return <h3>ryotamine@gmail.com</h3>;
+}
+
+// App class
 class App extends Component {
+  // Render app
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
     );
   }
 }
