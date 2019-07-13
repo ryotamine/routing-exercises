df200ad734a172d1d9feb49c543d673089a6eca9
Ryota Mine <ryotamine@gmail.com>
2019-07-13 23:19:49 +0000

    Express: Organize db files

--- express/knexfile.js
+++ express/knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
       password: "labber"
     },
     migrations: {
-      directory: "./migrations"
+      directory: "./db/migrations"
     },
     useNullAsDefault: true
   }
