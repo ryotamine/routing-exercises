e0406976c50b9eb63ec69ba39e7df14a0bdbcd13
Ryota Mine <ryotamine@gmail.com>
2019-07-13 22:36:51 +0000

    Check for unique email on registration form

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
+  validates_uniqueness_of :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
   validates :password, presence: true, length: { minimum: 8, maximum: 32 }
   has_secure_password
   
